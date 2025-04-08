27d4c7cbab1bac405ba5a46f1d152f67f06ba5c1
Matthew Shaw <matthew.shaw@str.us>
2025-04-08 12:07:36 -0400

    add example

---
+++ example.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import time
+
+import pylibsensors
+
+chips = pylibsensors.chips()
+#chips = pylibsensors.chips("coretemp-*")
+for chip_name,chip in chips.items():
+    for feature in chip.features:
+        for subfeature in chip[feature]:
+            print(f'{chip_name} / {feature} / {subfeature} = {chip[feature][subfeature].value()}')
+print()
+
+temp = chips['coretemp-isa-0000']['Package id 0']['temp1_input']
+volt = chips['BAT0-acpi-0']['in0']['in0_input']
+amps = chips['BAT0-acpi-0']['curr1']['curr1_input']
+
+try:
+    while True:
+       print(f'> {temp.value()} / {volt.value()} / {amps.value()}')
+       time.sleep(15)
+except KeyboardInterrupt:
+    pass
